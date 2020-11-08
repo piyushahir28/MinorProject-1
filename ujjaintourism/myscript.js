f4e6822ff9f707d5e3eacac024bdcd252183545d
Piyush Ahir <egpiyushahir@gmail.com>
2020-11-08 22:19:20 +0530

    Some Changes

--- ujjaintourism/myscript.js
+++ ujjaintourism/myscript.js
@@ -1,25 +1,102 @@
-var slideIndex = 0;
-showSlides();
+//Function to change the color of navbar when we scroll
+$(document).ready(function(){
+  $(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    if (scroll > 300) {
+      $("#navbar_top").css("background" , "white");
+      $("#navbar_top").css("box-shadow", "0 2px 2px -2px rgba(0,0,0,.2)");
+    }
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
+    else{
+      $("#navbar_top").css("background" , "transparent");   
+    }
+  })
+})
 
+//JS for navbar toggle whwn screen size is small
 $('.nav a').on('click', function(){
     $('.btn-navbar').click();
     $('.navbar-toggle').click();
     $('.navbar-toggler').click();
 });
+
+//Function to validate form
+function validateForm(frm){
+  var name=frm.name.value.trim();
+  if(name.length==0){
+    alert("Name Required");
+    frm.name.focus();
+    return false;
+  }
+  var e=0;
+  var v='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz .';
+  while(e<name.length){
+    if(v.indexOf(name.charAt(e))==-1){
+      alert("Only Alphabets Allowed");
+      frm.name.focus();
+      return false;
+    }
+    e++;
+  }
+  var email=frm.email.value.trim();
+  if(email.length==0){
+    alert("Email Required");
+    frm.email.focus();
+    return false;
+  }
+  var number=frm.number.value.trim();
+  if(number.length==0 ){
+    alert("Contact Number Required");
+    frm.number.focus();
+    return false;
+  }
+  if(number.length!=10 ){
+    alert("Invalid Contact Number");
+    frm.number.focus();
+    return false;
+  }
+  var e=0;
+  var v='0123456789';
+  while(e<number.length){
+    if(v.indexOf(number.charAt(e))==-1){
+      alert("Invalid Contact Number");
+      frm.number.focus();
+      return false;
+    }
+    e++;
+  }
+  var message=frm.message.value.trim();
+  if(message.length==0){
+    alert("Message Required");
+    frm.message.focus();
+    return false;
+  }
+  return true;
+}
+
+//Function for smooth scrolling
+$(document).ready(function(){
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+
+   // Make sure this.hash has a value before overriding default behavior
+  if (this.hash !== "") {
+
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 900, function(){
+
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+      });
+    } // End if
+  });
+})
